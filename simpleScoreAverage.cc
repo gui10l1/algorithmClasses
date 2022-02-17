4777710f54aedd59fc7d803b15a9194b7b28879f
gui10l1 <57021152+gui10l1@users.noreply.github.com>
2022-02-17 10:46:12 -0300

    Add: frequency calculation

--- simpleScoreAverage.cc
+++ simpleScoreAverage.cc
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-#define Average 2;
+#define Average 2
 #define NecessaryToPass 9.5
+#define Frequency 14
 
 int main() {
-	double firstScore, secondScore, totalScore;
+	double firstScore, secondScore, totalScore, frequencyPercentage, studentFrequency;
 	
 	printf("Insira a primeira nota: ");
 	scanf("%lf", &firstScore);
@@ -12,13 +13,17 @@ int main() {
 	printf("Insira a segunda nota: ");
 	scanf("%lf", &secondScore);
 	
+	printf("Insira a frequência do aluno: ");
+	scanf("%lf", &studentFrequency);
+	
+	frequencyPercentage = studentFrequency / Frequency * 100;
 	totalScore = (firstScore + secondScore) / Average;
 	
-	if (totalScore >= NecessaryToPass) {
-	    printf("Parabéns, passou!");
+	if (totalScore >= NecessaryToPass && frequencyPercentage == 100) {
+	    printf("Parabéns, passou com nota %.2lf e frequencia %.1lf", totalScore, frequencyPercentage);
 	    return 0;
 	} else {
-	    printf("Reprovou, tente novamente!");
+	    printf("Reprovou com nota %.2lf e frequencia %.1lf, tente novamente!", totalScore, frequencyPercentage);
 	    return 0;
 	}
 }
